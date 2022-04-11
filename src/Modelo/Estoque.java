f2b01988823d52c8833297f8e46fc930063d2b98
julianarpaz <julianarpaz@gmail.com>
2022-04-11 11:50:29 -0300

    Adição da classe estoque

---
+++ src/Modelo/Estoque.java
@@ -0,0 +1,20 @@
+package Modelo;
+
+public class Estoque {
+
+    Produto calça_jeans = new Produto("Calça jeans","001",80.0, 5);
+    Produto camisa_raglan = new Produto("Camisa Raglan","002", 55.0, 10);
+    Produto camiseta_regata_branca = new Produto("Camiseta Regata Branca", "003", 35.0, 12);
+
+    public Produto getCalça_jeans() {
+        return calça_jeans;
+    }
+
+    public Produto getCamisa_raglan() {
+        return camisa_raglan;
+    }
+
+    public Produto getCamiseta_regata_branca() {
+        return camiseta_regata_branca;
+    }
+}
