2bac29913b536161ddcd72ddb237e1e2f4a25fe3
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-04-12 01:48:16 -0300

    Adição de tratativas do cliente premium para final da compra e apresentação das informações no console de maneira mais interativa

--- src/Modelo/App.java
+++ src/Modelo/App.java
@@ -4,17 +4,19 @@ public class App {
 
     public static void main(String[] args) {
 
-        Estoque estoque = new Estoque(); //criação do objeto estoque
-        estoque.visualizacaoEstoque();   //visualização do estoque
-
-        Cliente cliente = new Cliente();
+    	Cliente cliente = new Cliente();
 
         Carrinho carrinho = new Carrinho (cliente);
-
+        
+        carrinho.abertura();
+    	
+    	Estoque estoque = new Estoque(); //criação do objeto estoque
+    	estoque.visualizacaoEstoque();
+              
         carrinho.perguntarAdicionar();
 
         carrinho.finalizacaoCompra();
-
+        
     }
 
 }
--- src/Modelo/Carrinho.java
+++ src/Modelo/Carrinho.java
@@ -18,6 +18,27 @@ public class Carrinho {
     public Carrinho(Cliente cliente) {
         this.cliente = cliente;
     }
+    
+    public void abertura() {
+    	System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("*                                                               *");
+		System.out.println("*                        FOLIA STORE                            *");
+		System.out.println("*                                                               *");
+		System.out.println("*                 Bem vinde a nossa loja!!!                     *");
+		System.out.println("*                                                               *");
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("\t\tNOME\t\t     CÓDIGO\t VALOR");
+    }
+    
+    public void fechamento() {
+    	System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("*                                                               *");
+		System.out.println("*                        FOLIA STORE                            *");
+		System.out.println("*                                                               *");
+		System.out.println("*       Muito obrigade pela preferência, volte sempre!!!        *");
+		System.out.println("*                                                               *");
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+    }
 
     private void adicionarItem(Produto produto) {
         this.carrinhoCliente.add(produto);
@@ -25,15 +46,27 @@ public class Carrinho {
 
     public void perguntarAdicionar() {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Qual item do nosso catálogo você deseja adicionar ao carrinho?");
+        System.out.println("\nQual item do nosso catálogo você deseja adicionar ao carrinho?");
         int codigoProduto = ler.nextInt();
         if (codigoProduto == 1) {
+        	System.out.println("-------------------------------------------------------------");
+			System.out.println("                     PRODUTO SELECIONADO                 ");
+			System.out.println("-------------------------------------------------------------");
+			System.out.println("\nNOME\t\t\t\t\t\t\tVALOR");
             adicionarItem(estoque.getProduto1());
 
         } else if (codigoProduto == 2) {
+        	System.out.println("-------------------------------------------------------------");
+			System.out.println("                     PRODUTO SELECIONADO                 ");
+			System.out.println("-------------------------------------------------------------");
+			System.out.println("\nNOME\t\t\t\t\t\t\tVALOR");
             adicionarItem(estoque.getProduto2());
 
         } else if (codigoProduto == 3) {
+        	System.out.println("-------------------------------------------------------------");
+			System.out.println("                     PRODUTO SELECIONADO                 ");
+			System.out.println("-------------------------------------------------------------");
+			System.out.println("\nNOME\t\t\t\t\t\t\tVALOR");
             adicionarItem(estoque.getProduto3());
 
         }
@@ -46,7 +79,7 @@ public class Carrinho {
 
     public void perguntarRetirar() {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Qual item deseja remover?");
+        System.out.println("\nQual item deseja remover?");
         int codigoProduto = ler.nextInt();
         if (codigoProduto == 1) {
             removerItem(estoque.getProduto1());
@@ -73,68 +106,85 @@ public class Carrinho {
     public void visualizacaoTotal() {
         precoCompra();
         for (Produto produtos : carrinhoCliente) {
-            System.out.println(produtos.getNome() + "\t\t\t" + NumberFormat.getCurrencyInstance().format(produtos.getPreco()));
+            System.out.println(" ====================================================================================");
+        	System.out.println("                                    NOTA FISCAL                                 ");
+        	System.out.println("");
+            System.out.println(produtos.getNome() + "\t\t" + NumberFormat.getCurrencyInstance().format(produtos.getPreco()));
+            System.out.println("");
         }
-        System.out.println("Total dos itens: " + NumberFormat.getCurrencyInstance().format(precoTotal));
-        System.out.println("Valor do frete: " + NumberFormat.getCurrencyInstance().format(frete));
-        System.out.println("Total a pagar: " + NumberFormat.getCurrencyInstance().format(precoTotal+frete));
+        System.out.println("Total dos itens: \t\t\t\t" + NumberFormat.getCurrencyInstance().format(precoTotal));
+        System.out.println("Valor do frete: \t\t\t\t" + NumberFormat.getCurrencyInstance().format(frete));
+        System.out.println("Total a pagar: \t\t\t\t\t" + NumberFormat.getCurrencyInstance().format(precoTotal+frete));
+        System.out.println("");
+        System.out.println(" ====================================================================================");
     }
 
+    public void limpa() {
+    	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
     private int escolhaPagamento(Pagamento pagamento) {
-        System.out.println("Método de pagamento escolhido foi " + pagamento.name());
+        System.out.println("\nMétodo de pagamento escolhido: " + pagamento.name());
         return pagamento.ordinal();
     }
-
+    
     public void finalizacaoCompra() {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Você deseja prosseguir com a finalizacao da compra? \n1-Sim\n2-Não");
+        System.out.println("\nVocê deseja finalizar sua compra? \n1-Sim\n2-Não");
         int continuar = ler.nextInt();
-        System.out.println("Você deseja se tornar cliente premium e obter benefícios?\n1-Sim\n2-Não");
-        int beneficios = ler.nextInt();
-        switch (beneficios) {
-            case 1:
-                this.cliente = new ClientePremium();
-                //this.cliente = (ClientePremium) cliente;
-                System.out.println();
-                System.out.println("Preencha seu email: ");
-                ler.nextLine();
-                String email = ler.nextLine();
-                cliente.setEmail(email);
-                cliente.checarEmail();
-                System.out.println("Preencha seu nome: ");
-                String nome = ler.nextLine();
-                ((ClientePremium) cliente).setNome(nome);
-                System.out.println("Aqui cliente premium ganha frete grátis.");
-                this.frete = 0;
-                break;
-            case 2:
-                this.frete = 20.0;
-                break;
-        }
         switch (continuar) {
             case 1:
-                System.out.println("Escolha o método de pagamento.\n1-Crédito\n2-Débito\n3-Boleto");
+            	System.out.println("\nVocê deseja se tornar cliente premium e obter benefícios?\n1-Sim\n2-Não");
+                int beneficios = ler.nextInt();
+                switch (beneficios) {
+                    case 1:
+                        this.cliente = new ClientePremium();
+                        //this.cliente = (ClientePremium) cliente;
+                        System.out.println();
+                        System.out.println("\nPreencha seu email: ");
+                        ler.nextLine();
+                        String email = ler.nextLine();
+                        cliente.setEmail(email);
+                        cliente.checarEmail();
+                        System.out.println("\nPreencha seu nome: ");
+                        String nome = ler.nextLine();
+                        ((ClientePremium) cliente).setNome(nome);
+                        System.out.println("\nAqui cliente premium ganha frete grátis.");
+                        this.frete = 0;
+                        break;
+                    case 2:
+                        this.frete = 20.0;
+                        break;
+                }
+                System.out.println("\nEscolha o método de pagamento.\n1-Crédito\n2-Débito\n3-Boleto");
                 int metodo = ler.nextInt();
                 switch (metodo) {
                     case 1:
                         escolhaPagamento(Pagamento.credito);
+                        limpa();
                         break;
                     case 2:
                         escolhaPagamento(Pagamento.debito);
+                        limpa();
                         break;
                     case 3:
                         escolhaPagamento(Pagamento.boleto);
+                        limpa();
                         break;
                     default:
-                        System.out.println("Método inválido.");
+                        System.out.println("\nMétodo inválido.");
                 }
                 visualizacaoTotal();
-                System.out.println("Compra concluída com sucesso!");
-                JOptionPane.showMessageDialog(null, "Compra finalizada com sucesso!");
+                System.out.println("\nCompra concluída com sucesso!");
+                fechamento();
+                //JOptionPane.showMessageDialog(null, "\nCompra finalizada com sucesso!");
                 break;
             case 2:
-                visualizacaoItens();
-                System.out.println("Você deseja adicionar ou remover itens?\n1-Adicionar\n2-Remover\n3-Sair do App");
+                System.out.println("-------------------------------------------------------------");
+    			System.out.println("                          SEU CARRINHO                       ");
+    			System.out.println("-------------------------------------------------------------");
+    			System.out.println("\nNOME\t\t\t\t\t\t\tVALOR");
+    			visualizacaoItens();
+                System.out.println("\nVocê deseja adicionar ou remover itens?\n1-Adicionar\n2-Remover\n3-Sair do App");
                 int resposta = ler.nextInt();
                 switch (resposta) {
                     case 1:
@@ -148,7 +198,7 @@ public class Carrinho {
                     case 3:
                         return;
                     default:
-                        System.out.println("Opção inválida.");
+                        System.out.println("\nOpção inválida.");
                 }
 
         }
